b6184ecd4378f2d4830bc63f4eacdc3a55dd7720
mikita <mikitakuzmichkin@gmail.com>
2024-01-22 12:17:06 +0300

    Added Baloon anim

---
+++ Assets/Scripts/UI/Balloon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace UI
+{
+    public class Balloon : MonoBehaviour
+    {
+        public float minHeight = 0f;
+        public float maxHeight = 5f;
+        public float minSpeed = 1f;
+        public float maxSpeed = 3f;
+        [SerializeField] private float _width = 2;
+
+        public void Spawn()
+        {
+            gameObject.SetActive(true);
+            float height = Random.Range(minHeight, maxHeight);
+            float speed = Random.Range(minSpeed, maxSpeed);
+            transform.localPosition = new Vector3(0f, height, 0f);
+            var pos = transform.position;
+            transform.DOLocalPath(new Vector3[]
+            {
+                new Vector3(-_width / 2f, height * Mathf.Sin(Random.Range(0f, 360f)), 0f),
+                new Vector3( 0, height * Mathf.Sin(Random.Range(0f, 360f)), 0f),
+                new Vector3( _width, height * Mathf.Sin(Random.Range(0f, 360f)), 0f)
+            }, speed, PathType.Linear).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                transform.localPosition = Vector3.zero;
+                gameObject.SetActive(false);
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/UI/BaloonManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public class BaloonManager : MonoBehaviour
+    {
+        public List<Balloon> balloons = new List<Balloon>();
+
+        void Start()
+        {
+            for (int i = 0; i < balloons.Count; i++)
+            {
+                balloons[i].gameObject.SetActive(false);
+            }
+        }
+
+        void Update()
+        {
+            var count = Random.Range(1, balloons.Count);
+            for (int i = 0; i < balloons.Count; i++)
+            {
+                if(balloons[i].gameObject.activeSelf)
+                {
+                    continue;
+                }
+                
+                balloons[i].Spawn();
+                count--;
+                if (count < 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
