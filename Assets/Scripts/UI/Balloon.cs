9875093dc1fe85fbfd548e6de116cc093514d8db
mikita <mikitakuzmichkin@gmail.com>
2024-01-22 13:46:50 +0300

    Fix balloons

--- Assets/Scripts/UI/Balloon.cs
+++ Assets/Scripts/UI/Balloon.cs
@@ -7,17 +7,17 @@ namespace UI
 {
     public class Balloon : MonoBehaviour
     {
-        public float minHeight = 0f;
-        public float maxHeight = 5f;
-        public float minSpeed = 1f;
-        public float maxSpeed = 3f;
+        [SerializeField] private float _minHeight = 0f;
+        [SerializeField] private float _maxHeight = 5f;
+        [SerializeField] private float _minSpeed = 1f;
+        [SerializeField] private float _maxSpeed = 3f;
         [SerializeField] private float _width = 2;
 
         public void Spawn()
         {
             gameObject.SetActive(true);
-            float height = Random.Range(minHeight, maxHeight);
-            float speed = Random.Range(minSpeed, maxSpeed);
+            float height = Random.Range(_minHeight, _maxHeight);
+            float speed = Random.Range(_minSpeed, _maxSpeed);
             transform.localPosition = new Vector3(0f, height, 0f);
             var pos = transform.position;
             transform.DOLocalPath(new Vector3[]
@@ -25,7 +25,7 @@ namespace UI
                 new Vector3(-_width / 2f, height * Mathf.Sin(Random.Range(0f, 360f)), 0f),
                 new Vector3( 0, height * Mathf.Sin(Random.Range(0f, 360f)), 0f),
                 new Vector3( _width, height * Mathf.Sin(Random.Range(0f, 360f)), 0f)
-            }, speed, PathType.Linear).SetEase(Ease.Linear).OnComplete(() =>
+            }, speed, PathType.CatmullRom).SetEase(Ease.Linear).OnComplete(() =>
             {
                 transform.localPosition = Vector3.zero;
                 gameObject.SetActive(false);
